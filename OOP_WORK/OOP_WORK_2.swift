051f8af8d3cdda34f0eba3d6cd7f064f7e8cb7eb
tubaaNur <yesiltastubanur@gmail.com>
2023-06-30 21:06:46 +0300

    test

---
+++ OOP_WORK/OOP_WORK_2.swift
@@ -0,0 +1,8 @@
+//
+//  OOP_WORK_2.swift
+//  OOP_WORK
+//
+//  Created by Tuba Nur  on 30.06.2023.
+//
+
+import Foundation
--- OOP_WORK/main.swift
+++ OOP_WORK/main.swift
@@ -1,5 +1,50 @@
 
 import Foundation
 
-print("Hello, World!")
+//ARRAY Ortalama Hesaplama
+/*
+var dizi=[1,2,3,4,5]
+var toplam=0
+for sayi in dizi{
+    toplam = toplam+sayi
+}
+print("Toplam = \(toplam) " )
+print("Ortalama = \(toplam / dizi.count) ") */
+
+//ARRAY İÇERİK DEĞİŞTİRME
+//
+//var dizi : [String] = ["Tuba", "İbo", "Başar","Nur"]
+//dizi [2] = "Kaymak"
+//print("Yeni dizi : \(dizi) ")
+
+//var dizi=[Int]()
+//for _ in 1...10{
+//    let rastgeleSayi=arc4random_uniform(10)
+//    dizi.append(Int(rastgeleSayi))
+//
+//}
+//dizi.sort()
+//print(dizi)
+
+//SET Çalışması
+
+//var dizi : Set<Int> = [1, 2 ,3]
+//for i in dizi{
+//    print("Dizi \(dizi)")
+//}
+//dizi.sorted()
+//print(dizi)
+//dizi.removeAll()
+
+//Dictionary
+
+//var dic:[Int:String]=[1: "Tuba", 2: "Başar"]
+//print(dic)
+//print("Dictionary eleman sayısı : \(dic.count)")
+//dic[2]="Başar2"
+//print(dic)ü
+//var numbers:[Int:String]=[10: "Tuba", 20: "Başar"]
+//var sonuc = numbers.filter({$0.key > 10})
+//print(sonuc)
+
 
