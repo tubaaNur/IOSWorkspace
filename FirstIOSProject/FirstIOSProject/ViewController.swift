f985d32f66a13cf9aec39e2c447965ea238e4b81
tubaaNur <yesiltastubanur@gmail.com>
2023-07-02 23:55:47 +0300

    First IOS Project completed

---
+++ FirstIOSProject/FirstIOSProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstIOSProject
+//
+//  Created by Tuba Nur  on 2.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
