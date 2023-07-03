30b91de941112fce18dee88ab572c8752433346a
tubaaNur <yesiltastubanur@gmail.com>
2023-07-04 01:08:50 +0300

    Login Page Project Implemented

---
+++ LoginPage/LoginPage/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Tuba Nur  on 4.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func UserNameAc(_ sender: UITextField) {
+    }
+    
+    @IBAction func passwordAc(_ sender: Any) {
+        
+    }
+    @IBAction func loginbutton(_ sender: Any) {
+        
+        
+    }
+    override func viewDidLoad() {
+       
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ deneme12/deneme12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  deneme12
+//
+//  Created by Tuba Nur  on 4.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
