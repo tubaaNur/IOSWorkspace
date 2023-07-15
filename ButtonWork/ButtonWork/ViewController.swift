f1813354f1d5c7bc6aa5af00762920ef36f30a89
tubaaNur <yesiltastubanur@gmail.com>
2023-07-15 22:07:16 +0300

    ActivityIndicatorView is worked

--- ButtonWork/ButtonWork/ViewController.swift
+++ ButtonWork/ButtonWork/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var sliderLabel: UISlider!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     @IBOutlet weak var Label: UILabel!
     @IBOutlet weak var TextFieldButton: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        indicator.isHidden = true
         // Do any additional setup after loading the view.
     }
 
@@ -27,6 +29,14 @@ class ViewController: UIViewController {
     @IBAction func clicks(_ sender: Any) {
     }
     @IBAction func slidercontrol(_ sender: Any) {
+    }
+    @IBAction func buttonone(_ sender: Any) {
+        indicator.startAnimating()
+        indicator.isHidden = false
+    }
+    @IBAction func buttontwo(_ sender: Any) {
+        indicator.startAnimating()
+        indicator.isHidden = true
     }
 }
 
